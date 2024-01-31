aac927bb5398d655d642feebdc7240e8a981967f
adeveloper-wq <dajesch@gmail.com>
2024-01-31 17:34:25 +0100

    Prepare for dynamic api key insertion via environment

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,16 @@
 FROM prom/prometheus:v2.32.1
 
-ADD prometheus.yml /etc/prometheus/
+COPY prometheus.yml /etc/prometheus/prometheus.yml
 
+COPY start_prometheus.sh /usr/local/bin/start_prometheus.sh
+
+# Install sed (used to set API Keys)
+# RUN apt-get update && apt-get install -y sed
+
+# Set permissions
+USER root
+RUN chmod +x /usr/local/bin/start_prometheus.sh
+RUN chmod 777 /etc/prometheus/prometheus.yml
+USER nobody
+
+ENTRYPOINT [ "/bin/sh", "-c", "/usr/local/bin/start_prometheus.sh" ]
---
+++ start_prometheus.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PROMETHEUS_CONFIG=$(cat /etc/prometheus/prometheus.yml)
+# Replace the API key placeholders in the config file with the actual API keys
+PROMETHEUS_CONFIG_UPDATED=$(echo "${PROMETHEUS_CONFIG/TRACKING_SERVICE_API_KEY/$TRACKING_SERVICE_API_KEY}")
+PROMETHEUS_CONFIG_UPDATED=$(echo "${PROMETHEUS_CONFIG_UPDATED/BIKER_SWARM_MONITOR_API_KEY/$BIKER_SWARM_MONITOR_API_KEY}")
+# Write updated config to /etc/prometheus/prometheus.yml
+echo "$PROMETHEUS_CONFIG_UPDATED" > /etc/prometheus/prometheus.yml
+
+/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --storage.tsdb.retention.time=1y
