31b41aaeb5c73b501638af812f6d122eecd232ae
adeveloper-wq <dajesch@gmail.com>
2024-05-27 15:36:07 +0200

    Remove outdated config

--- start_prometheus.sh
+++ start_prometheus.sh
@@ -3,7 +3,6 @@
 PROMETHEUS_CONFIG=$(cat /etc/prometheus/prometheus.yml)
 # Replace the API key placeholders in the config file with the actual API keys
 PROMETHEUS_CONFIG_UPDATED=$(echo "${PROMETHEUS_CONFIG/TRACKING_SERVICE_API_KEY/$TRACKING_SERVICE_API_KEY}")
-PROMETHEUS_CONFIG_UPDATED=$(echo "${PROMETHEUS_CONFIG_UPDATED/BIKER_SWARM_MONITOR_API_KEY/$BIKER_SWARM_MONITOR_API_KEY}")
 PROMETHEUS_CONFIG_UPDATED=$(echo "${PROMETHEUS_CONFIG_UPDATED/LOAD_SERVICE_API_KEY/$LOAD_SERVICE_API_KEY}")
 # Write updated config to /etc/prometheus/prometheus.yml
 echo "$PROMETHEUS_CONFIG_UPDATED" > /etc/prometheus/prometheus.yml
